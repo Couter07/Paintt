b649f3297da2db7ec80425b54d7e50df1da12620
qanh-02 <24130014@st.hcmuaf.edu.vn>
2025-02-23 00:17:09 +0700

    thêm chức năng vẽ , vẽ màu và màu nền của Paint

--- src/paint/ShapeState.java
+++ src/paint/ShapeState.java
@@ -1,15 +1,35 @@
 package paint;
 
 import java.awt.*;
+import paint.ShapeState;
 
-public class ShapeState {
+public class ShapeState  {
+    public int group;
     private Shape shape; // Hình vẽ lưu trữ
-
-    public ShapeState(Shape shape) {
+    private Color color; // Màu của nét vẽ
+    private int strokeWidth; // Độ dày của nét vẽ
+//    public int group = 0;
+    public ShapeState(Shape shape, Color color, int strokeWidth, int group) {
         this.shape = shape;
+        this.color = color;
+        this.strokeWidth = strokeWidth;
+        this.group = group;
+
+
+    }
+
+    public ShapeState(Shape shape, Color currentColor, int strokeWidth) {
     }
 
     public Shape getShape() {
         return shape;
+    }
+
+    public Color getColor() {return color;
+    }
+
+    public int getStrokeWidth() {return strokeWidth;
+    }
+    public int getGroup() {return group;
     }
 }
