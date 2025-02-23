78d51c71f7e541a2baad86ab18927663736299fb
Cảnh <24130029@st.hcmuaf.edu.vn>
2025-02-23 09:47:14 +0700

    Fix all code + draw shape, line, redo, undo, eraser, stroke

--- src/paint/ShapeState.java
+++ src/paint/ShapeState.java
@@ -1,35 +1,33 @@
 package paint;
 
 import java.awt.*;
-import paint.ShapeState;
 
 public class ShapeState  {
-    public int group;
     private Shape shape; // Hình vẽ lưu trữ
     private Color color; // Màu của nét vẽ
     private int strokeWidth; // Độ dày của nét vẽ
-//    public int group = 0;
+    private int group; // Nhóm của hình vẽ
+
     public ShapeState(Shape shape, Color color, int strokeWidth, int group) {
         this.shape = shape;
         this.color = color;
         this.strokeWidth = strokeWidth;
         this.group = group;
-
-
-    }
-
-    public ShapeState(Shape shape, Color currentColor, int strokeWidth) {
     }
 
     public Shape getShape() {
         return shape;
     }
 
-    public Color getColor() {return color;
+    public Color getColor() {
+        return color;
     }
 
-    public int getStrokeWidth() {return strokeWidth;
+    public int getStrokeWidth() {
+        return strokeWidth;
     }
-    public int getGroup() {return group;
+
+    public int getGroup() {
+        return group;
     }
 }
